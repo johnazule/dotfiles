6fbdb55db2b25fb93de0a9e2997393dbda8d8fd5
Jeremy Spedding <johnazule@gmail.com>
2025-08-10 13:48:23 +1000

    Rust Analyzer Fixes and Markdown plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -456,6 +456,27 @@ require("lazy").setup({
 			},
 			opts_extend = { "sources.default" }
 		},
+		{
+			'MeanderingProgrammer/render-markdown.nvim',
+			dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+			---@module 'render-markdown'
+			---@type render.md.UserConfig
+			opts = {
+				heading = {
+					backgrounds = {},
+				},
+				completions = { blink = { enabled = true } },
+			},
+		},
+		{
+			'AntonVanAssche/md-headers.nvim',
+			dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
+			version = '*',
+			opts = {},
+			ft = { 'markdown' }, -- Load only for markdown files.
+		}
 
 		-- {
 		-- 	'saghen/blink.pairs',
--- nvim/lsp/rust_analyzer.lua
+++ nvim/lsp/rust_analyzer.lua
@@ -1,3 +1,31 @@
+vim.lsp.commands['rust-analyzer.gotoLocation'] = function(command, ctx)
+	local client = vim.lsp.get_client_by_id(ctx.client_id)
+	if client then
+		vim.lsp.util.show_document(command.arguments[1], client.offset_encoding or 'utf-8')
+	end
+end
+
+vim.lsp.commands['rust-analyzer.showReferences'] = function(_)
+	vim.lsp.buf.implementation()
+end
+
+vim.lsp.commands['rust-analyzer.runSingle'] = function(command)
+	local r = command.arguments[1]
+	local cmd = { 'cargo', unpack(r.args.cargoArgs) }
+	if r.args.executableArgs and #r.args.executableArgs > 0 then
+		vim.list_extend(cmd, { '--', unpack(r.args.executableArgs) })
+	end
+
+	local proc = vim.system(cmd, { cwd = r.args.cwd })
+
+	local result = proc:wait()
+
+	if result.code == 0 then
+		vim.notify(result.stdout, vim.log.levels.INFO)
+	else
+		vim.notify(result.stderr, vim.log.levels.ERROR)
+	end
+end
 --- @type vim.lsp.ClientConfig
 return {
 	cmd = { "rust-analyzer" },
@@ -17,14 +45,14 @@ return {
 				}
 			}
 		},
-		experiemental = {
+		experimental = {
 			serverStatusNotification = true,
 			hoverActions = true,
 			codeActionGroup = true,
 			commands = {
 				commands = {
 					'rust-analyzer.showReferences',
-					'rust-analyzer.debugSingle',
+					-- 'rust-analyzer.debugSingle',
 					'rust-analyzer.runSingle',
 					'rust-analyzer.gotoLocation',
 					'rust-analyzer.triggerParameterHints',
@@ -104,3 +132,54 @@ return {
 	}
 
 }
+
+-- -- Override the runSingle command on the client
+-- vim.lsp.commands['rust-analyzer.runSingle'] = function(command)
+--   local r = command.arguments[1]
+--   local cmd = { 'cargo', unpack(r.args.cargoArgs) }
+--   if r.args.executableArgs and #r.args.executableArgs > 0 then
+--     vim.list_extend(cmd, { '--', unpack(r.args.executableArgs) })
+--   end
+--
+--   local proc = vim.system(cmd, { cwd = r.args.cwd })
+--
+--   local result = proc:wait()
+--
+--   if result.code == 0 then
+--     vim.notify(result.stdout, vim.log.levels.INFO)
+--   else
+--     vim.notify(result.stderr, vim.log.levels.ERROR)
+--   end
+-- end
+-- return
+--   --- @type vim.lsp.ClientConfig
+--   {
+--     cmd = { "rust-analyzer" },
+--     filetypes = { "rust" },
+--     capabilities = {
+--       experimental = {
+--         commands = {
+--           commands = {
+--             'rust-analyzer.showReferences',
+--              'rust-analyzer.runSingle',
+--              'rust-analyzer.debugSingle',
+--           }
+--         }
+--       }
+--     },
+--     settings = {
+--       ['rust-analyzer'] = {
+--         lens = {
+--           enable = true,
+--           run = { enable = true },
+--           implementations = { enable = true },
+--           references = {
+--             adt = { enable = true },
+--             method = { enable = true},
+--             trait = { enable = true },
+--             enumVariant = { enable = true }
+--           }
+--         },
+--       }
+--     }
+--   }
